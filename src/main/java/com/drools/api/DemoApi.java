4af2ad383b7d976422f7a850531dda723831d7a3
ketangote <ketan.gote@metamagic.in>
2019-03-13 18:09:07 +0530

    Changes: Update

--- src/main/java/com/drools/api/DemoApi.java
+++ src/main/java/com/drools/api/DemoApi.java
@@ -4,7 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,10 +20,10 @@ public class DemoApi {
 	private DroolsRuntimeService droolsRuntimeService;
 	
 	
-	@GetMapping("/execute")
-	public ResponseEntity<Object> executeRule() throws Exception{
-		System.out.println("*******IN EXECUTE RULE************");
-		Object newobj = droolsRuntimeService.runRule("6c2e77ea-5bde-4200-903b-606fc40b74b3"," {\"id\":\"ID-1234567890\",\"customerId\":\"CustomerId-1234567890\",\"customerType\":\"GOLD\",\"totalAmount\":2000.0,\"discount\":0,\"payableAmount\":2000.0}");
+	@PostMapping("/execute")
+	public ResponseEntity<Object> executeRule(@RequestBody String body) throws Exception{
+		System.out.println("*******IN EXECUTE RULE************"+body);
+		Object newobj = droolsRuntimeService.runRule("6c2e77ea-5bde-4200-903b-606fc40b74b3",body);
 		System.out.println(newobj);
 		return new ResponseEntity<Object>(newobj, HttpStatus.OK);
 	}
